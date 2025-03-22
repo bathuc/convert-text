61f3383a6a31b4161c649241d7c768f2f090e642
bathuc <lebathuc@gmail.com>
2025-03-22 21:24:49 +0700

    update highlight code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,18 @@ class HomeController extends Controller
 {
     public function convertText(Request $request)
     {
-        $convertText = htmlentities($request->convert_text);
+        $convertText = $request->convert_text;
+        $convertText = preg_replace('/</', '&lt;', $convertText);
+        $convertText = preg_replace('/>/', '&gt;', $convertText);
+        $convertText = preg_replace('/"/', '&quot;', $convertText);
+        $convertText = preg_replace("/'/", '&#39;', $convertText);
 
-        return view('convert', compact('convertText'));
+        //$convertText = htmlentities($request->convert_text);
+        $language = $request->language;
+
+        return view('convert', compact(
+            'convertText', 
+            'language',
+        ));
     }
 }
--- resources/views/convert.blade.php
+++ resources/views/convert.blade.php
@@ -6,20 +6,74 @@
         
         <link rel="icon" href="favicon.png">
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <!-- Bootstrap 5.2.3 -->
+        <link 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
+        <script
+            src="https://code.jquery.com/jquery-3.7.1.min.js"
+            integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+            crossorigin="anonymous"></script>
+
+            <!-- bootstrap 5.2.3 -->
+        <script 
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 		
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" 
+            crossorigin="anonymous"></script>
     </head>
     <body>
         <form method="post" action="{{ route('convert-text') }}" class="mt-5">
             {{ csrf_field() }}
             <textarea  name="convert_text" rows="7" class="form-control col-10">{{ old('convert_text', request()->convert_text) }}</textarea>
-            <button type="submit" class="btn btn-primary btn-submit mt-3">Submit</button>
+            
+            <div class="d-flex mt-3">
+                <input type="hidden" name="language" value="{{ old('language', request()->language) }}">
+                <button type="button" class="btn btn-primary btn-get-raw-text me-3">Get Raw Text</button>
+                <button type="button" class="btn btn-success btn-get-code">Get Color Code</button>
+            </div>
         </form>
+
+       
     
-        <pre class="mt-3">
-            <p>-------------------</p><br>
+<p class="mt-3">-------------------</p>
+<p class="fw-bold text-success">
+    @if (!empty($language))
+        Code is write in language: {{ $language }}
+    @else 
+        Raw String
+    @endif
+</p>
+<br>
+
+<div class="wrapper">
+    <pre>
+        @if(!empty($language))
+            <code class="hljs">
+                {!! trim($convertText) !!}
+            </code>
+        @else
             {!! trim($convertText) !!}
-            <br><p>-------------------</p>
-        </pre>
+        @endif
+    </pre>
+</div>
+
+<br>
+<p>-------------------</p>
+
+
+
+    <script>
+        $(document).ready(function() {
+            $('.btn-get-raw-text').click(function() {
+                $('input[name="language"]').val('');
+                $('form').submit();
+            });
+            $('.btn-get-code').click(function() {
+                $('input[name="language"]').val('1');
+                $('form').submit();
+            });
+        })
+    </script>
     </body>
 </html>
